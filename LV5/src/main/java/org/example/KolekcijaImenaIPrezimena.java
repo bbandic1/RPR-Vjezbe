fb75d0b4d04930205dd63203eda7418f5331efb0
bbandic1 <148074216+bbandic1@users.noreply.github.com>
2023-11-18 22:31:41 +0100

    Work in Progress...

---
+++ LV5/src/main/java/org/example/KolekcijaImenaIPrezimena.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class KolekcijaImenaIPrezimena extends KolekcijaImena{
+    private ArrayList<String> imena;
+    private ArrayList<String> prezimena;
+
+    public int getIndexNajduzegPara()
+    {
+        return 1;
+    }
+
+}
